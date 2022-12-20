a095149c85b5a19cbe11bf2ac949b304900f9b87
Lyfmeno <hugolevipro@gmail.com>
2022-12-20 13:07:56 +0100

    parsing now check empty line after the map started

--- src/parsing/parse_map.c
+++ src/parsing/parse_map.c
@@ -6,7 +6,7 @@
 /*   By: hlevi <hlevi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 09:37:27 by hlevi             #+#    #+#             */
-/*   Updated: 2022/12/20 12:58:11 by hlevi            ###   ########.fr       */
+/*   Updated: 2022/12/20 13:07:53 by hlevi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,7 +82,7 @@ static int	parse_map_close(t_data *data)
 		j = 0;
 		while (data->map.arr[i][j])
 		{
-			if (data->map.arr[i][j] == '0')
+			if (is_charset(data->map.arr[i][j], "NSEW0"))
 				if (parse_map_zero(data, i, j))
 					return (-1);
 			j++;
