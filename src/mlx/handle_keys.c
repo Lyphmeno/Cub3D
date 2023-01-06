4644f8fce08cfc44e07c8f51c1afab017f037e86
lyfmeno <hugolevipro@gmail.com>
2023-01-06 09:00:43 +0100

    Issues with map col

--- src/mlx/handle_keys.c
+++ src/mlx/handle_keys.c
@@ -6,7 +6,7 @@
 /*   By: hlevi <hlevi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 11:18:54 by hlevi             #+#    #+#             */
-/*   Updated: 2023/01/04 15:45:04 by hlevi            ###   ########.fr       */
+/*   Updated: 2023/01/05 12:47:01 by hlevi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,31 +55,20 @@ int	release_key(int keycode, t_data *data)
 
 int	handle_mouse(int x, int y, t_data *data)
 {
-	int		oldx;
-	double	speed;
+	int		delta_x;
+    double	speed;
 
-	(void)y;
-	speed = 0.02;
-	oldx = data->mousex;
-	if (data->mousex == -1)
-	{
-		data->mousex = x;
-		return (0);
-	}
-	data->mousex = x;
-	if (data->mousex == oldx)
-		return (0);
-	if (data->mousex > oldx)
-		data->player->dir += speed;
-	if (data->mousex < oldx)
-		data->player->dir -= speed;
-	data->mousex = x;
-	return (0);
+    (void)y;
+    speed = 0.02;
+    delta_x = x - data->mousex;
+    data->player->dir += delta_x * speed;
+    data->mousex = x;
+    return 0;
+
 }
 
 int	handle_key(int keycode, t_data *data)
 {
-	printf("[%d]\n", keycode);
 	if (keycode == 65307)
 		escape(data);
 	if (keycode == 65365)
--- src/mlx/move.c
+++ src/mlx/move.c
@@ -6,7 +6,7 @@
 /*   By: hlevi <hlevi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 14:44:19 by hlevi             #+#    #+#             */
-/*   Updated: 2023/01/04 15:12:04 by hlevi            ###   ########.fr       */
+/*   Updated: 2023/01/05 12:45:05 by hlevi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,76 +15,140 @@
 
 #define SPEED 0.05
 
+static int	check_col(t_data *data, int x, int y)
+{
+	printf("col = %d,%d\n", x, y);
+	if (data->map->arr[y][x] == '1')
+		return (1);
+	return (0);
+}
+
 static void	move_up(t_data *data)
 {
 	double	posx;
 	double	posy;
+	double	incx;
+	double	incy;
+	double	dist;
 
-	posx = data->player->posx + cos(data->player->dir) * SPEED;
-	posy = data->player->posy + sin(data->player->dir) * SPEED;
-	if (data->keys->kup)
+	posx = data->player->posx;
+	posy = data->player->posy;
+	incx = cos(data->player->dir) * SPEED;
+	incy = sin(data->player->dir) * SPEED;
+	dist = sqrt(incx * incx +incy * incy);
+	while (dist > 0)
 	{
-		if (posx > 0 && posx < data->map->width)
-			data->player->posx += cos(data->player->dir) * SPEED;
-		if (posy > 0 && posy < data->map->height)
-			data->player->posy += sin(data->player->dir) * SPEED;
+		posx += incx;
+		posy += incy;
+		if (check_col(data, posx, posy))
+		{
+			posx -= incx;
+			posy -= incy;
+			break;
+		}
+		dist -= SPEED;
 	}
+	data->player->posx = posx;
+	data->player->posy = posy;
 }
 
 static void	move_down(t_data *data)
 {
 	double	posx;
 	double	posy;
+	double	incx;
+	double	incy;
+	double	dist;
 
-	posx = data->player->posx - cos(data->player->dir) * SPEED;
-	posy = data->player->posy - sin(data->player->dir) * SPEED;
-	if (data->keys->kdown)
+	posx = data->player->posx;
+	posy = data->player->posy;
+	incx = cos(data->player->dir) * SPEED;
+	incy = sin(data->player->dir) * SPEED;
+	dist = sqrt(incx * incx +incy * incy);
+	while (dist > 0)
 	{
-		if (posx > 0 && posx < data->map->width)
-			data->player->posx -= cos(data->player->dir) * SPEED;
-		if (posy > 0 && posy < data->map->height)
-			data->player->posy -= sin(data->player->dir) * SPEED;
+		posx -= incx;
+		posy -= incy;
+		if (check_col(data, posx, posy))
+		{
+			posx += incx;
+			posy += incy;
+			break;
+		}
+		dist -= SPEED;
 	}
+	data->player->posx = posx;
+	data->player->posy = posy;
 }
 
 static void	move_left(t_data *data)
 {
 	double	posx;
 	double	posy;
+	double	incx;
+	double	incy;
+	double	dist;
 
-	posx = data->player->posx - cos(data->player->dir + M_PI / 2) * SPEED;
-	posy = data->player->posy - sin(data->player->dir + M_PI / 2) * SPEED;
-	if (data->keys->kleft)
+	posx = data->player->posx;
+	posy = data->player->posy;
+	incx = cos(data->player->dir + M_PI / 2) * SPEED;
+	incy = sin(data->player->dir + M_PI / 2) * SPEED;
+	dist = sqrt(incx * incx +incy * incy);
+	while (dist > 0)
 	{
-		if (posx > 0 && posx < data->map->width)
-			data->player->posx -= cos(data->player->dir + M_PI / 2) * SPEED;
-		if (posy > 0 && posy < data->map->height)
-			data->player->posy -= sin(data->player->dir + M_PI / 2) * SPEED;
+		posx -= incx;
+		posy -= incy;
+		if (check_col(data, posx, posy))
+		{
+			posx += incx;
+			posy += incy;
+			break;
+		}
+		dist -= SPEED;
 	}
+	data->player->posx = posx;
+	data->player->posy = posy;
 }
 
 static void	move_right(t_data *data)
 {
 	double	posx;
 	double	posy;
+	double	incx;
+	double	incy;
+	double	dist;
 
-	posx = data->player->posx + cos(data->player->dir + M_PI / 2) * SPEED;
-	posy = data->player->posy + sin(data->player->dir + M_PI / 2) * SPEED;
-	if (data->keys->kright)
+	posx = data->player->posx;
+	posy = data->player->posy;
+	incx = cos(data->player->dir + M_PI / 2) * SPEED;
+	incy = sin(data->player->dir + M_PI / 2) * SPEED;
+	dist = sqrt(incx * incx +incy * incy);
+	while (dist > 0)
 	{
-		if (posx > 0 && posx < data->map->width)
-			data->player->posx += cos(data->player->dir + M_PI / 2) * SPEED;
-		if (posy > 0 && posy < data->map->height)
-			data->player->posy += sin(data->player->dir + M_PI / 2) * SPEED;
+		posx += incx;
+		posy += incy;
+		if (check_col(data, posx, posy))
+		{
+			posx -= incx;
+			posy -= incy;
+			break;
+		}
+		dist -= SPEED;
 	}
+	data->player->posx = posx;
+	data->player->posy = posy;
 }
 
 void	move(t_data *data)
 {
-	move_up(data);
-	move_down(data);
-	move_left(data);
-	move_right(data);
+	if (data->keys->kup)
+		move_up(data);
+	if (data->keys->kdown)
+		move_down(data);
+	if (data->keys->kleft)
+		move_left(data);
+	if (data->keys->kright)
+		move_right(data);
 	if (data->keys->kdirr)
 		data->player->dir -= SPEED;
 	if (data->keys->kdirl)
